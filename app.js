585a3d1e2c5eba5295834f16cd83cf1069edafd5
Jose Gutierrez <josgutcej@gmail.com>
2021-11-09 19:02:48 +0100

    linkeado

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const app = express();
 
-
 app.use(express.static("public"));
 
-
 app.get("/", (request, response) => {
-  '
-  res.sendFile(__dirname + "/views/home.html");
+  response.sendFile(__dirname + "/views/home.html");
 });
-
 
 app.get("/about", (request, response) => {
-  res.sendFile(__dirname + "/views/about.html");
+  response.sendFile(__dirname + "/views/about.html");
 });
-
 
 app.get("/gallery", (request, response) => {
-  res.sendFile(__dirname + "/views/gallery.html");
+  response.sendFile(__dirname + "/views/gallery.html");
 });
 
-app.listen(3000, () => {
-});
+app.listen(3000, () => {});
